7bda41c80201dfa03e241f322a28b38199fd10e5
Sark_oo7 <kosinick01@gmail.com>
2025-06-30 08:03:26 +0100

    Add user name display to welcome message in index view

--- DEMO/views/index.view.php
+++ DEMO/views/index.view.php
@@ -4,7 +4,7 @@
 
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    Welcome to the demo page! This is a simple layout using Tailwind CSS. Below are some details about the book you are reading.
+    Welcome, <span class="font-bold text-2xl"><?= $_SESSION["user"]["name"] ?? ""?></span> to the demo page! This is a simple layout using Tailwind CSS. Below are some details about the book you are reading.
   </div>
 </main>
 
